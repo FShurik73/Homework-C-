a11e0339654ad7656c93a8fc8bd4dea849caff8f
Alexandr Fokin <shurik_f@rambler.ru>
2022-10-22 21:49:20 +0300

    third homework

---
+++ Homework lecture 3/Task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+bool ValidateNumber(int num)
+{
+    if (num < 10000 || num > 100000)
+    {
+        System.Console.WriteLine("Число не пятизначное, введите другое число");
+        return false;
+    }
+    return true;
+}
+int num = Prompt("Введите пятизначное число > ");
+int value = num;
+if (ValidateNumber(num))
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result * 10 + num % 10;
+        num /= 10;
+    }
+    if (value == result)
+    {
+        System.Console.WriteLine($"{value} -> да");
+    }
+    else
+    {
+        System.Console.WriteLine($"{value} -> нет");
+    }
+}
+
---
+++ Homework lecture 3/Task 3/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задача 3: Напишите программу, которая принимает на вход число (N)
+//   и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27. 
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27 
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   
+    return int.Parse(Console.ReadLine());   
+    }
+int N = Prompt("Введите число больше 0>  ");
+int value = N;
+int num = 1; 
+while (num <= N) 
+{
+    System.Console.Write($"{num}^3 -> {num*num*num}, ");
+    num++; 
+}
+// for (int i = 1; i<=N; i++)  // цикл for
+// {
+//    System.Console.Write($"{i}^3 -> {Math.Pow(i,3)}, ");
+// }
+
+
+
