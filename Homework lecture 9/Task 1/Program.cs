c1b98e41b088431e810ea2ed66423f1e39d9fd13
Alexandr Fokin <shurik_f@rambler.ru>
2022-11-27 17:30:54 +0300

    Homework lecture 9

---
+++ Homework lecture 9/Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+//  которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void PrintNumbersEven(int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    if (n % 2 == 0)
+        PrintNumbersEven(m, n - 2);
+    else
+        PrintNumbersEven(m, n - 1);
+    if (n % 2 == 0)
+    {
+        Console.WriteLine(n + " ");
+    }
+}
+
+int m = 1;
+int n = 9;
+PrintNumbersEven(m, n);
+
+
+
---
+++ Homework lecture 9/Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+int m = 1;
+int n = 15;
+SumMN(m, n);
+Console.WriteLine(SumMN(m - 1, n));
